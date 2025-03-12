c5060353e96d2fa6a8bdab6d5ad89e990f955674
DimaxOne <vestor@inbox.ru>
2025-03-12 22:56:30 +0700

    Create CrystalShop.cs

---
+++ CrystalShop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystalsInBag = 0;
+            int crystalPrice = 10;
+            int goldsInBag;
+            int crystalsForPurchase;
+
+            Console.Write("Сколько у Вас золота? ");
+            goldsInBag = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Скалько кристаллов Вам нужно? ");
+            crystalsForPurchase = Convert.ToInt32(Console.ReadLine());
+
+            goldsInBag -= crystalsForPurchase * crystalPrice;
+            crystalsInBag += crystalsForPurchase;
+
+            Console.WriteLine($"У Вас в сумке {goldsInBag} золота и {crystalsInBag} кристаллов.");
+        }
+    }
+}
