4f1c5f5dfe1e3a32a5eb6c7d0f3fff297447c1cb
DimaxOne <vestor@inbox.ru>
2025-03-12 18:23:20 +0700

    Create ImagesInRow.cs

---
+++ ImagesInRow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int images = 52;
+            int imagesInRow = 3;
+
+            int filledRow = 52 / 3;
+            int remainingImage = 52 % 3;
+
+            Console.WriteLine($"Полностью заполненных рядов - {filledRow}, картинки сверх меры - {remainingImage}.");
+        }
+    }
+}
