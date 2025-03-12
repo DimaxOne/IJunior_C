5d641e1e7f6ba8444004e7960ac267ec41dc5213
DimaxOne <vestor@inbox.ru>
2025-03-13 02:29:31 +0700

    Create Sequence.cs

---
+++ Sequence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = 103;
+            int step = 7;
+
+            for (int i = 5; i <= maxNumber;)
+            {
+                Console.Write(i + " ");
+                i += step;
+            }
+        }
+    }
+}
