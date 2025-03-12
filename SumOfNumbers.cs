bf4a8d8e75fc6ca8de633a19d9ee63e8430508ce
DimaxOne <vestor@inbox.ru>
2025-03-13 02:56:49 +0700

    Create SumOfNumbers.cs

---
+++ SumOfNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int firstDivisor = 3;
+            int secondDivisor = 5;
+            int maxRandomNumber = 100;
+            int sum = 0;
+            int number;
+            bool canDivideWithoutRemainder;
+
+            number = rand.Next(0, maxRandomNumber);
+
+            for (int i = 0; i <= number; i++)
+            {
+                canDivideWithoutRemainder = i % firstDivisor == 0 || i % secondDivisor == 0;
+
+                if (canDivideWithoutRemainder)
+                    sum += i;
+            }
+
+            Console.WriteLine($"Для числа {number} сумма чисел равна: {sum}.");
+        }
+    }
+}
