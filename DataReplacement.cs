8e27e2a8f7b671daf8b9dda6d45049b605192665
DimaxOne <vestor@inbox.ru>
2025-03-12 22:43:17 +0700

    Create DataReplacement.cs

---
+++ DataReplacement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Смирнов";
+            string surname = "Евгений";
+            string firstCap = "кофе";
+            string secondCap = "чай";
+            string template;
+
+            Console.WriteLine($"Ваша фамилия - {surname}, Вас зовут - {name}.");
+            template = name;
+            name = surname;
+            surname = template;
+            Console.WriteLine($"Произведены корректировки. Ваша фамилия - {surname}, Вас зовут - {name}.");
+
+            Console.WriteLine($"В первой чашке сейчас {firstCap}, во второй чашке {secondCap}.");
+            template = firstCap;
+            firstCap = secondCap;
+            secondCap = template;
+            Console.WriteLine($"Произведен перелив содержимого. В первой чашке сейчас {firstCap}, во второй чашке {secondCap}.");
+        }
+    }
+}
