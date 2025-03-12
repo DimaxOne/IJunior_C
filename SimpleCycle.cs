f4f8080d8942032604d8924d4c20cadf2485c201
DimaxOne <vestor@inbox.ru>
2025-03-13 02:02:31 +0700

    Create SimpleCycle.cs

---
+++ SimpleCycle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SimpleCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int repeats;
+
+            Console.Write("Введите сообщение для повтора: ");
+            message = Console.ReadLine();
+            Console.Write("Введите количество повторов: ");
+            repeats = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < repeats; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
