5581bad37a40481711464479877204d38de2f2ce
DimaxOne <vestor@inbox.ru>
2025-03-12 23:17:23 +0700

    Create Polyclinic.cs

---
+++ Polyclinic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Polyclinic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int admissionDuration = 10;
+            int minutesOnHour = 60;
+            int peopleInLine;
+            int hours;
+            int minutes;
+
+            Console.Write("Людей в очереди: ");
+            peopleInLine = Convert.ToInt32(Console.ReadLine());
+
+            hours = peopleInLine * admissionDuration / minutesOnHour;
+            minutes = peopleInLine * admissionDuration % minutesOnHour;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hours} часа и {minutes} минут.");
+        }
+    }
+}
