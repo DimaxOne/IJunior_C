4c2b4295b39f20e1b4c2b3ed2a4344842324998b
DimaxOne <vestor@inbox.ru>
2025-03-12 07:40:55 +0700

    Create Variables.cs

---
+++ Variables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName = "Tomas";
+            string clan = "Wolfs";
+            int health = 100;
+            int armor = 15;
+            int damage = 19;
+            float moveSpeed = 2.4f;
+            float attackSpeed = 4.7f;
+            bool isEnable = true;
+            bool canAttack = false;
+            char playerLogo = '@';
+        }
+    }
+}
