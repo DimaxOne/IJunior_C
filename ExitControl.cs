ffd54650e46ea8cb06b77e1789185d0c71aa8099
DimaxOne <vestor@inbox.ru>
2025-03-13 02:16:45 +0700

    Create ExitControl.cs

---
+++ ExitControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExitControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isEnable = true;
+            string exitСommand = "exit";
+            string userСommand;
+
+            while (isEnable)
+            {
+                Console.Write($"\nДля выхода введите {exitСommand}. \nВведите Вашу команду: ");
+                userСommand = Console.ReadLine();
+
+                if (userСommand == exitСommand)
+                    isEnable = false;
+                else
+                    Console.WriteLine(userСommand);
+            }
+        }
+    }
+}
