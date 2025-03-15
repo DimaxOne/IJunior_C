09c9a86b8eb0e143e65885e8727d9e216663bb2a
DimaxOne <vestor@inbox.ru>
2025-03-15 08:22:55 +0700

    Create NameOutput.cs

---
+++ NameOutput.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NameOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int extraLength = 2;
+            int rowCount = 3;
+            int middleRow;
+            string name;
+            char symbol;
+
+            Console.Write("Введите Ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите символ: ");
+            symbol = Convert.ToChar(Console.ReadLine());
+
+            middleRow = rowCount / 2;
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < name.Length + extraLength; j++)
+                {
+                    if (i == middleRow)
+                    {
+                        Console.Write(symbol + name + symbol);
+                        break;
+                    }
+
+                    Console.Write(symbol);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
