045fa6ab1e660897e872ad73b227c9f73f29179c
DimaxOne <vestor@inbox.ru>
2025-03-15 11:48:21 +0700

    Create Multiples.cs

---
+++ Multiples.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Multiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minimumRandomValue = 10;
+            int maximumRandomValue = 25;
+            int minimumValue = 50;
+            int maximumValue = 150;
+            int sum = 0;
+            int number;
+
+            number = random.Next(minimumRandomValue, maximumRandomValue + 1);
+
+            for (int i = number; i <= maximumValue; i += number)
+            {
+                if (i >= minimumValue)
+                    sum++;
+            }
+
+            Console.WriteLine($"Количество числел кратных {number} в диапазоне от {minimumValue} " +
+                $"до {maximumValue} составляет {sum}.");
+        }
+    }
+}
