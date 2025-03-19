828fd3c6bfb07123faa77edcf9a4bc1893ff91d7
DimaxOne <vestor@inbox.ru>
2025-03-19 20:53:09 +0700

    Update LocalHighs.cs

--- LocalHighs.cs
+++ LocalHighs.cs
@@ -16,23 +16,17 @@ namespace LocalHighs
                 numbers[i] = random.Next(minimumRandomValue, maximumRandomValue + 1);
             }
 
-            for (int i = 0; i < numbers.Length; i++)
+            if (numbers[0] > numbers[1])
+                Console.Write(numbers[0] + " ");
+
+            for (int i = 1; i < numbers.Length - 1; i++)
             {
-                if (i == 0)
-                {
-                    if (numbers[i] > numbers[i + 1])
-                        Console.Write(numbers[i] + " ");
-                }
-                else if (i == numbers.Length - 1)
-                {
-                    if (numbers[numbers.Length - 1] > numbers[numbers.Length - 2])
-                        Console.Write(numbers[numbers.Length - 1] + " ");
-                }
-                else if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
-                {
-                        Console.Write(numbers[i] + " ");
-                }
+                if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
+                    Console.Write(numbers[i] + " ");
             }
+
+            if (numbers[numbers.Length - 1] > numbers[numbers.Length - 2])
+                Console.Write(numbers[numbers.Length - 1] + " ");
         }
     }
 }
