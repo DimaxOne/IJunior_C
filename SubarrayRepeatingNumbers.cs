b3209bc3536c18a5003605bd2091d050a10ef78d
DimaxOne <vestor@inbox.ru>
2025-03-20 21:36:03 +0700

    Create SubarrayRepeatingNumbers.cs

---
+++ SubarrayRepeatingNumbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SubarrayRepeatingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] numbers = new int[30];
+            int maximumRandomValue = 3;
+            int minimumRandomValue = 1;
+            int currentRepetition = 0;
+            int maximumRepetition = 0;
+            int maximumNumberRepeated = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minimumRandomValue, maximumRandomValue + 1);
+            }
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    currentRepetition++;
+                }
+                else
+                {
+                    if (maximumRepetition < currentRepetition)
+                    {
+                        maximumRepetition = currentRepetition;
+                        maximumNumberRepeated = numbers[i];
+                    }
+
+                    currentRepetition = 0;
+                }
+            }
+
+            Console.Write("{");
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                Console.Write(numbers[i] + ", ");
+            }
+
+            Console.Write($"{numbers[numbers.Length - 1]}}} - число {maximumNumberRepeated} повторяется {maximumRepetition + 1} раза подряд.");
+        }
+    }
+}
