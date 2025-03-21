8c22d32193277524efa95b6aaada5e542112d4c7
DimaxOne <vestor@inbox.ru>
2025-03-21 09:27:02 +0700

    Update SubarrayRepeatingNumbers.cs

--- SubarrayRepeatingNumbers.cs
+++ SubarrayRepeatingNumbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SubarrayRepeatingNumbers
 {
@@ -10,41 +11,48 @@ namespace SubarrayRepeatingNumbers
             int[] numbers = new int[30];
             int maximumRandomValue = 3;
             int minimumRandomValue = 1;
-            int currentRepetition = 0;
+            int defaultRepetition = 1;
             int maximumRepetition = 0;
             int maximumNumberRepeated = 0;
+            int matchesOnFirstRepetition = 2;
+            bool isRepetition = false;
+            int currentRepetition;
 
             for (int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = random.Next(minimumRandomValue, maximumRandomValue + 1);
             }
 
+            currentRepetition = defaultRepetition;
+
             for (int i = 0; i < numbers.Length - 1; i++)
             {
                 if (numbers[i] == numbers[i + 1])
                 {
+                    isRepetition = true;
                     currentRepetition++;
-                }
-                else
-                {
+
                     if (maximumRepetition < currentRepetition)
                     {
                         maximumRepetition = currentRepetition;
                         maximumNumberRepeated = numbers[i];
                     }
-
-                    currentRepetition = 0;
+                }
+                else
+                {
+                    currentRepetition = defaultRepetition;
                 }
             }
 
-            Console.Write("{");
-
-            for (int i = 0; i < numbers.Length - 1; i++)
+            if(isRepetition)
             {
-                Console.Write(numbers[i] + ", ");
+                Console.WriteLine("{" + string.Join(", ", numbers) + 
+                    $"}} - число { maximumNumberRepeated} повторяется { maximumRepetition} раза подряд.");
             }
-
-            Console.Write($"{numbers[numbers.Length - 1]}}} - число {maximumNumberRepeated} повторяется {maximumRepetition + 1} раза подряд.");
+            else
+            {
+                Console.WriteLine("Повторений нет.");
+            }
         }
     }
 }
