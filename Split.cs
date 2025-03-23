c8f7b8be79c9b93186d97acd0bd351f820f4422c
DimaxOne <vestor@inbox.ru>
2025-03-23 15:03:05 +0700

    Update Split.cs

--- Split.cs
+++ Split.cs
@@ -9,9 +9,10 @@ namespace Split
             string message = "Дана строка с текстом, используя метод строки String.Split() " +
                 "получить массив слов, которые разделены пробелом в тексте и вывести массив, " +
                 "каждое слово с новой строки.";
+            char separationSymbol = ' ';
             string[] words;
 
-            words = message.Split(' ');
+            words = message.Split(separationSymbol);
 
             foreach (string word in words)
             {
