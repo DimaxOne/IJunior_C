73fb11f163ebfa3430c07db68d943f0757c6a448
DimaxOne <vestor@inbox.ru>
2025-03-24 11:14:12 +0700

    Create ShiftingArrayValues.cs

---
+++ ShiftingArrayValues.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ShiftingArrayValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] numbers = new int[15];
+            int maximumRandomValue = 30;
+            int minimumRandomValue = 0;
+            int positionShiftIndex;
+            int templateNumber;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minimumRandomValue, maximumRandomValue + 1);
+            }
+
+            Console.Write("Дан массив: " + string.Join(" ", numbers) +
+                "\nНа сколько позиций влево его нужно сдвинуть? ");
+
+            positionShiftIndex = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < positionShiftIndex; i++)
+            {
+                templateNumber = numbers[0];
+
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    numbers[j] = numbers[j + 1];
+                }
+
+                numbers[numbers.Length - 1] = templateNumber;
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
