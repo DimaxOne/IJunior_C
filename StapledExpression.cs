5484dad3de975c4f99a5de950215fc45b15b3957
DimaxOne <vestor@inbox.ru>
2025-03-25 06:01:59 +0700

    Create StapledExpression.cs

---
+++ StapledExpression.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace StapledExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbols = "((()))((((()))))";
+            char openSymbol = '(';
+            char closeSymbol = ')';
+            int openSymbolCount = 0;
+            int closeSymbolCount = 0;
+            int maximumDepth = 0;
+            bool isCorrectExpression = true;
+            int currentDepth;
+
+            foreach (char symbol in symbols)
+            {
+                if (symbol == openSymbol)
+                    openSymbolCount++;
+                else if (symbol == closeSymbol)
+                    closeSymbolCount++;
+
+                if (openSymbolCount >= closeSymbolCount)
+                {
+                    currentDepth = openSymbolCount - closeSymbolCount;
+
+                    if (currentDepth > maximumDepth)
+                        maximumDepth = currentDepth;
+                }
+                else
+                {
+                    isCorrectExpression = false;
+                }
+            }
+
+            if (openSymbolCount != closeSymbolCount)
+                isCorrectExpression = false;
+
+            if (isCorrectExpression)
+            {
+                Console.WriteLine("Это корректное скобочное выражение.");
+                Console.WriteLine($"Максимальная глубина вложенности скобок - {maximumDepth}.");
+            }
+            else
+            {
+                Console.WriteLine("Выражение некорректно.");
+            }
+        }
+    }
+}
