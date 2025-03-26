1ae72417689f7f043ec03c73130c4fd7b601d5d9
DimaxOne <vestor@inbox.ru>
2025-03-26 15:01:09 +0700

    Create ReadInt.cs

---
+++ ReadInt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReadInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetNumber());
+        }
+
+        private static int GetNumber()
+        {
+            bool isNumber = false;
+            int number = 0;
+            string userInput;
+
+            while(!isNumber)
+            {
+                Console.Write("Введите число: ");
+                userInput = Console.ReadLine();
+                
+                if(int.TryParse(userInput, out number))
+                    isNumber = true;
+            }
+
+            return number;
+        }
+    }
+}
