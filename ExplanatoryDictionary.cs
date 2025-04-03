4f21f73447e6962600607b808370de81ecd29163
DimaxOne <vestor@inbox.ru>
2025-04-03 21:22:51 +0700

    Update ExplanatoryDictionary.cs

--- ExplanatoryDictionary.cs
+++ ExplanatoryDictionary.cs
@@ -7,25 +7,15 @@ namespace ExplanatoryDictionary
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> numbers = new Dictionary<string, int>();
-            string[] keyNumbers = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" };
-            int[] valueNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Dictionary<string, int> numbers = new Dictionary<string, int>() { { "One", 1 }, { "Two" , 2 }, { "Three", 3 },
+                {"Four", 4 }, {"Five", 5 }, {"Six",6 }, {"Seven", 7 },{"Eight", 8 },{"Nine", 9 },{"Ten", 10 } };
 
-            FillDictionary(numbers, keyNumbers, valueNumbers);
             ShowNumber(GetUserInput("Напишите словом какое число вам нужно: "), numbers);
-        }
-
-        private static void FillDictionary(Dictionary<string, int> dictionary, string[] keyNumbers, int[] valueNumbers)
-        {
-            for (int i = 0; i < keyNumbers.Length; i++)
-            {
-                dictionary.Add(keyNumbers[i].ToLower(), valueNumbers[i]);
-            }
         }
 
         private static void ShowNumber(string userInput, Dictionary<string, int> numbers)
         {
-            if (numbers.TryGetValue(userInput.ToLower(), out int value))
+            if (numbers.TryGetValue(userInput, out int value))
                 Console.WriteLine(value);
             else
                 Console.WriteLine("Такого слова нет.");
