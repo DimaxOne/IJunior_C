ed82baa197cee9e696f3fcf921d674e3294808a6
DimaxOne <vestor@inbox.ru>
2025-04-16 19:10:56 +0700

    Create WorkWithClasses.cs

---
+++ WorkWithClasses.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WorkWithClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Voin", 120, 26, 19);
+
+            player.ShowStats();
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _health;
+        private int _armor;
+        private int _damage;
+
+        public Player(string name, int health, int armor, int damage)
+        {
+            _name = name;
+            _health = health;
+            _armor = armor;
+            _damage = damage;
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine($"У игрока {_name} имеется: {_health} здоровья, {_armor} брони и {_damage} урона.");
+        }
+    }
+}
