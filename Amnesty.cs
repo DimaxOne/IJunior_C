bee712352f7c54fe63091c6c4aabc2142ea373b3
DimaxOne <vestor@inbox.ru>
2025-07-30 13:39:00 +0700

    Create Amnesty.cs

---
+++ Amnesty.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Amnesty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arstotzka arstotzka = new Arstotzka();
+
+            arstotzka.ShowPrisoners();
+            arstotzka.GrantAmnesty();
+            arstotzka.ShowPrisoners();
+        }
+    }
+
+    class Arstotzka
+    {
+        List<Prisoner> _prisoners;
+
+        public Arstotzka()
+        {
+            _prisoners = new List<Prisoner>();
+
+            FillPrisoners();
+        }
+
+        public void GrantAmnesty()
+        {
+            string crimeForAmnesty = "Антиправительственное";
+            char separatorSymbol = '*';
+            int symbolCount = 100;
+
+            string separator = "\n\n" + new string(separatorSymbol, symbolCount) + "\n\n";
+
+            Console.WriteLine(separator + "В нашей стране произошла амнистия! " +
+                "Список заключеных изменен, многие были освобождены." + separator);
+
+            var amnestyPrisoners = _prisoners.Where(prisioner => prisioner.Crime != crimeForAmnesty).ToList();
+            _prisoners = amnestyPrisoners;
+        }
+
+        public void ShowPrisoners()
+        {
+            foreach (Prisoner prisoner in _prisoners)
+            {
+                prisoner.ShowInfo();
+            }
+        }
+
+        private void FillPrisoners()
+        {
+            _prisoners.Add(new Prisoner("Беляков Климент Тихонович", "Мошенничество"));
+            _prisoners.Add(new Prisoner("Гурьев Ермак Митрофанович", "Антиправительственное"));
+            _prisoners.Add(new Prisoner("Шарапов Игнатий Вадимович", "Хулиганство"));
+            _prisoners.Add(new Prisoner("Калинин Владлен Геннадиевич", "Разбой"));
+            _prisoners.Add(new Prisoner("Колесников Тимур Пётрович", "Антиправительственное"));
+            _prisoners.Add(new Prisoner("Ильин Мечеслав Валерьянович", "Антиправительственное"));
+            _prisoners.Add(new Prisoner("Иванов Венедикт Мэлсович", "Разбой"));
+            _prisoners.Add(new Prisoner("Ильин Мартын Яковович", "Мошенничество"));
+            _prisoners.Add(new Prisoner("Зайцев Павел Евсеевич", "Антиправительственное"));
+            _prisoners.Add(new Prisoner("Доронин Вилли Тихонович", "Хулиганство"));
+            _prisoners.Add(new Prisoner("Харитонов Августин Тимофеевич", "Антиправительственное"));
+            _prisoners.Add(new Prisoner("Федотов Любомир Даниилович", "Мошенничество"));
+            _prisoners.Add(new Prisoner("Беляев Леонид Станиславович", "Антиправительственное"));
+            _prisoners.Add(new Prisoner("Марков Лаврентий Мартынович", "Разбой"));
+            _prisoners.Add(new Prisoner("Баранов Лев Макарович", "Хулиганство"));
+        }
+    }
+
+    class Prisoner
+    {
+        private string _fullName;
+
+        public Prisoner(string fullName, string crime)
+        {
+            _fullName = fullName;
+            Crime = crime;
+        }
+
+        public string Crime { get; private set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Имя: {_fullName}. Вид преступления - {Crime}.");
+        }
+    }
+}
